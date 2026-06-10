2f73b8e3eee5bee32fbdcdd08ff621b44f468c02
Rindra-Aniko <rindra.aniko@gmail.com>
2026-06-10 19:44:17 +0700

    setting siremaps 3

--- app/robots.ts
+++ app/robots.ts
@@ -1,14 +1,14 @@
 import { MetadataRoute } from 'next';
 
-export const dynamic = 'force-static';
-
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/admin/', '/api/'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/admin', '/api'],
+      },
+    ],
     sitemap: 'https://kiswah.id/sitemap.xml',
   };
 }
